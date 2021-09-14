7eb700400514c9210b58d69c8760e7d24233224d
LMChong <90686359+LMChong@users.noreply.github.com>
2021-09-14 17:23:35 +0800

    Add files via upload

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, render_template, redirect, url_for
+import json
+import requests
+
+app = Flask(__name__)
+
+@app.route('/home', methods=['GET'])
+def home ():
+    return(render_template('index.html'))
+
+@app.route('/about', methods=['GET'])
+def about_us():
+    return ("Hello, I am a robot from UBTECH Robotics, nice to meet you")
+
+@app.route('/weather', methods=['GET'])
+def weather():
+    city = request.args.get('q')
+    url = 'http://api.openweathermap.org/data/2.5/weather?appid=dc20d3160e7d504175d202850b79b74b&units=metric&q='
+    base_url = url + city
+    response= requests.get(base_url).json()
+    #data = response.json()
+    tmp = response["main"]["temp"]
+    description = response["weather"][0]["description"]
+    return (render_template('index.html', city=city, weather = description, tmp = tmp))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
